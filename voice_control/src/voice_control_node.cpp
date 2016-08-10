8fa05bea6a7814345865baabe24bc0fcfb3ff142
Waywrong <wlhsu304@gmail.com>
2016-08-10 16:09:47 +0800

    speech_msg1

--- voice_control/src/voice_control_node.cpp
+++ voice_control/src/voice_control_node.cpp
@@ -1,16 +1,22 @@
 #include "ros/ros.h"
 #include "speech_recognition_msgs/SpeechRecognitionCandidates.h"
 
+void speechCallback(const speech_recognition_msgs::SpeechRecognitionCandidatesPtr& msg)
+{
+  
+  ROS_INFO("I heard: [%s]", msg->transcript.size());
+}
+
 int main(int argc, char **argv)
 {
   ros::init(argc, argv, "voice_control");
   
   ros::NodeHandle nh;
   ros::Rate r(10);
-    
+  ros::Subscriber speech_Sub = nh.subscribe("/Tablet/voice", 10, speechCallback);
   while(ros::ok())
   {
-    //rvizFloorPlan();//later
+    
     r.sleep();
     ros::spinOnce();
     
